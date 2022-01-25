56a93658deed3de9607fd88ef0d6a7d2f00c201b
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2022-01-25 16:08:37 +0300

    when the button is tapped textview changes

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener{
-            val toast = Toast.makeText(this,"Dice Rolled!" , Toast.LENGTH_SHORT).show()
-           // toast.show();
+            val resultTextView : TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
+
         }
     }
 }
